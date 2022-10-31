b00f9c66d945284708d0a0762870423329b0f30b
Cong Vu <116829489+CongLiv@users.noreply.github.com>
2022-10-31 18:43:59 +0700

    Update Rhombus.java

--- BT Buoi 1/myPack/Rhombus.java
+++ BT Buoi 1/myPack/Rhombus.java
@@ -4,10 +4,10 @@ public class Rhombus {
     
     public float a, b;
 
-    public double perimeter() {
+    public double Perimeter() {
         return 4 * Math.sqrt(a * a / 4 + b * b / 4);
     }
-    public double area() {
+    public double Area() {
         return 0.5 * a * b;
     }
 }
